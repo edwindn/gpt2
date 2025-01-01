e8779a54e2e21bdb0b52824915f8411021175353
edwindn <edn.work@proton.me>
2025-01-01 21:12:32 +0000

    Update model_ddp.py

--- model_ddp.py
+++ model_ddp.py
@@ -183,7 +183,7 @@ def test_run(gpt, device):
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '12356'
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def cleanup():
