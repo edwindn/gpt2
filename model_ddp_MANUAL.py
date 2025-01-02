4b89206850487eded5b0a86b7d1a993ab3d33cf0
edwindn <edn.work@proton.me>
2025-01-02 01:07:58 +0000

    Update model_ddp_MANUAL.py

--- model_ddp_MANUAL.py
+++ model_ddp_MANUAL.py
@@ -195,7 +195,7 @@ def test_run(gpt, device):
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 def cleanup():
     dist.destroy_process_group()
